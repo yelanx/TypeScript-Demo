9cd7ed686c0ccf2d750004e25ae0fba066f06025
muhongsong <muhongsong@myhexin.com>
2021-06-07 11:37:13 +0800

    new demo

---
+++ src/js/jsBasemant.js
@@ -0,0 +1,14 @@
+function baseConverter (decNumber ,base){
+    let num = decNumber;
+    let digist = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let resultArr = [];
+    let temp;
+    if( base <2 || base >36){return;}
+    while ( num > 0){
+        temp = Math.floor(num % base);
+        resultArr.push(temp);
+        num = Math.floor(num / base);
+    }
+    return resultArr.map(item => digist[item]).reverse().join("");
+}
+console.log(baseConverter(1024,2));
